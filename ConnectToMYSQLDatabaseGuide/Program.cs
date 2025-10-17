e4fcd25d3885ffcde03b5e773d371a13ddaa6850
Al Stone <astone@heckgrammar.co.uk>
2025-10-17 10:32:56 +0100

    SELECT and INSERT

---
+++ ConnectToMYSQLDatabaseGuide/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySqlConnector;
+
+namespace ConnectToMYSQLDatabaseGuide
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection c = new MySqlConnection("server=SERVER;uid=USERNAME;pwd=PASSWORD;database=DATABASENAME");
+            c.Open();
+            string SQL = "SELECT * FROM YOURTABLE";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = c;
+            cmd.CommandText = SQL;
+            MySqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                Console.WriteLine(r["YOURCOLUMNNAME"].ToString());
+            }
+            c.Close();
+            c.Open();
+            SQL = "INSERT INTO YOURTABLE (YOURCOLUMNNAME) VALUES ('YOURVALUE')";
+            cmd.CommandText = SQL;
+            cmd.ExecuteNonQuery();
+
+
+        }
+    }
+}
